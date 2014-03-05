541c174e8523f87600d9e54551320574eed2e110
lemonslice96 <lemonslice96@gmail.com>
2014-03-05 09:54:38 -0500

    your mom

---
+++ Number Guessing Game/Number Guessing Game/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Number_Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int guess = 50;
+            Console.WriteLine("Is your number {0}? h, l, c", guess);
+            input = Console.ReadLine();
+            if (input == "h") ;
+        }
+    }
+}
