2ce889a349568729f29d57f169daadb49ea1d9dc
lemonslice96 <lemonslice96@gmail.com>
2014-03-05 10:10:16 -0500

    my mom

--- Number Guessing Game/Number Guessing Game/Program.cs
+++ Number Guessing Game/Number Guessing Game/Program.cs
@@ -10,10 +10,26 @@ namespace Number_Guessing_Game
         static void Main(string[] args)
         {
             string input;
+            while (true)
             int guess = 50;
             Console.WriteLine("Is your number {0}? h, l, c", guess);
             input = Console.ReadLine();
-            if (input == "h") ;
+            if (input == "h");
+            {
+                Console.WriteLine(" Is your number 75? h, l, c");
+                input = Console.ReadLine();
+                if (input == "h");
+                {
+                    Console.WriteLine(" Is your number 87? h, l, c");
+                    input = Console.ReadLine();
+                    if (input == "h") ;
+                    {
+                        Console.WriteLine(" Is your number 94? h, l, c");
+                        input = Console.ReadLine();
+                        if (input == "h") ;
+                    }
+                }
+            }
         }
     }
 }
